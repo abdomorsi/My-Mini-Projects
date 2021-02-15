1f61a1fd2f0e2c0d3fea4ddf8d32c429d91f1929
abdomorsi <36486531+abdomorsi@users.noreply.github.com>
2021-02-15 12:32:59 +0200

    Update servlet1.java

--- BankingSystem/src/java/servlet1.java
+++ BankingSystem/src/java/servlet1.java
@@ -1,12 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
-Name : Abdelrahman Mohammed Ahmed Morsy 
-ID : 20170149
-group : cs-is 2
 
-*/
 
 import java.io.IOException;
 import java.io.PrintWriter;
